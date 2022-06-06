339be2271a5ab9ab63d8146c44f6f079387a73ae
Andydevs <akanshul97@gmail.com>
2022-06-06 19:44:41 -0400

    Initialize app code

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: './app/index.js',
+    devServer: {
+        static: path.join(__dirname, 'public'),
+        compress: true,
+        port: 8080,
+        open: true
+    },
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react'
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+}
